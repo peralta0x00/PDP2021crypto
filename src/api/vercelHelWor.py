7ef94e3c3d35ffe5d759d9333cc5d1ec4e504c4c
kevinparalta <peralta0x00@gmail.com>
2021-07-02 11:08:50 -0700

    using sample encoded string

--- src/api/vercelHelWor.py
+++ src/api/vercelHelWor.py
@@ -7,5 +7,5 @@ class testHandler(BaseHTTPRequestHandler):
       self.send_response(200);
       self.send_header('Content-type', 'text/plain')
       self.end_headers()
-      self.wfile.write(str("hello world").encode())
+      self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
       return
