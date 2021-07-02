76be79538e8d80af08abe738a74b99602b3448c2
kevinparalta <peralta0x00@gmail.com>
2021-07-02 10:57:04 -0700

    adding test files for vercel import

---
+++ src/api/vercelHelWor.py
@@ -0,0 +1,11 @@
+from http import HTTPStatus
+from http.server import BaseHTTPRequestHandler
+
+class testHandler(BaseHTTPRequestHandler):
+   //get, as reminder, simply requests data; doesn't modify
+   def do_GET(self):
+      self.send_response(200);
+      self.send_header('Content-type', 'text/plain')
+      self.end_headers()
+      self.wfile.write(str("hello world").encode())
+      return
