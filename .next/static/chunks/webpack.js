1fec18f7ea7266503e7d98c0ef06b18e6e31c0d8
kevinparalta <peralta0x00@gmail.com>
2021-07-29 15:35:50 -0700

    modifying required packages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3d652a106f7fe9974b9a"; }
+/******/ 		__webpack_require__.h = function() { return "09ab8942068d049fd9da"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c4fe0d5cc4d6b3b00f25.hot-update.js
+++ .next/static/webpack/webpack.c4fe0d5cc4d6b3b00f25.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b4f4fcd3add80fe3f555"; }
+/******/ 	__webpack_require__.h = function() { return "b24693a143ed0f6ee124"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
